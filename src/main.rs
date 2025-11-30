36bdeb61ecd6308611a57e4793cafd36ac70fe0b
RobinJehn <robin.jehn@gmx.de>
2025-11-30 17:25:43 +0100

    handling quotes

--- src/main.rs
+++ src/main.rs
@@ -49,57 +49,131 @@ fn find_exec(cmd: &str) -> Option<PathBuf> {
     return None;
 }
 
+#[derive(Copy, Clone)]
+enum ParseState {
+    InsideDoubleQuote,
+    InsideSingleQuote,
+    Outside,
+    AfterEscapeChar,
+}
+
+fn parse_input(input: &str) -> Vec<String> {
+    let mut tokens: Vec<String> = Vec::new();
+
+    let mut state = ParseState::Outside;
+    let mut prev_esc_char_state = state;
+    let mut token = String::new();
+    for c in input.chars() {
+        match c {
+            '\'' => match state {
+                ParseState::InsideSingleQuote => state = ParseState::Outside,
+                ParseState::Outside => state = ParseState::InsideSingleQuote,
+                ParseState::AfterEscapeChar => {
+                    state = prev_esc_char_state;
+                    token.push(c);
+                }
+                _ => token.push(c),
+            },
+            '\\' => match state {
+                ParseState::AfterEscapeChar => {
+                    token.push(c);
+                    state = prev_esc_char_state;
+                }
+                _ => {
+                    prev_esc_char_state = state;
+                    state = ParseState::AfterEscapeChar;
+                    token.push(c);
+                }
+            },
+            '\"' => match state {
+                ParseState::AfterEscapeChar => {
+                    state = prev_esc_char_state;
+                    token.push(c);
+                }
+                ParseState::InsideDoubleQuote => state = ParseState::Outside,
+                ParseState::Outside => state = ParseState::InsideDoubleQuote,
+                _ => token.push(c),
+            },
+            ' ' => match state {
+                ParseState::AfterEscapeChar => {
+                    state = prev_esc_char_state;
+                    token.push(c);
+                }
+                ParseState::InsideSingleQuote => token.push(c),
+                ParseState::InsideDoubleQuote => token.push(c),
+                _ => {
+                    state = ParseState::Outside;
+                    if !token.is_empty() {
+                        tokens.push(token);
+                        token = String::new();
+                    }
+                }
+            },
+            _ => token.push(c),
+        }
+    }
+
+    if !token.is_empty() {
+        tokens.push(token);
+    }
+
+    return tokens;
+}
+
 fn main() {
     while true {
         print!("$ ");
         io::stdout().flush().unwrap();
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim_end().to_string();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim_end().to_string();
+        let tokens = parse_input(&input);
+        let cmd = tokens[0].to_string();
 
         // Handle commands
         let set = HashSet::from(["exit", "type", "echo", "pwd", "cd"]);
-        if command == "exit" {
+        if cmd == "exit" {
             return;
-        } else if command == "pwd" {
+        } else if cmd == "pwd" {
             let cwd = env::current_dir().unwrap();
             println!("{}", cwd.display());
-        } else if let Some(echo) = command.strip_prefix("echo ") {
-            println!("{}", echo);
-        } else if let Some(dir) = command.strip_prefix("cd ") {
+        } else if cmd == "echo" {
+            println!("{}", tokens[1]);
+        } else if cmd == "cd" {
             let home = env::var("HOME").unwrap_or_default();
-            let dir = dir.replace("~", &home);
+            let dir = tokens[1].replace("~", &home);
             match env::set_current_dir(&dir) {
                 Ok(_) => {}
                 Err(_) => println!("cd: {}: No such file or directory", dir),
             }
-        } else if let Some(type_cmd) = command.strip_prefix("type ") {
-            if set.contains(type_cmd) {
-                println!("{} is a shell builtin", type_cmd);
+        } else if cmd == "type" {
+            if set.contains(tokens[1].as_str()) {
+                println!("{} is a shell builtin", tokens[1]);
                 continue;
             }
 
-            match find_exec(type_cmd) {
-                Some(path) => println!("{} is {}", type_cmd, path.display()),
-                None => println!("{}: not found", type_cmd),
+            match find_exec(&tokens[1]) {
+                Some(path) => println!("{} is {}", tokens[1], path.display()),
+                None => println!("{}: not found", tokens[1]),
             }
         } else {
-            let args: Vec<&str> = command.split_whitespace().collect();
-
-            if args.is_empty() {
-                println!("{}: command not found", command);
+            if tokens.len() == 1 {
+                println!("{}: command not found", cmd);
             }
 
-            match find_exec(args[0]) {
+            match find_exec(&cmd) {
                 Some(_) => {
-                    let output = Command::new(args[0])
-                        .args(&args[1..])
+                    let output = Command::new(cmd)
+                        .args(&tokens[1..])
                         .output()
                         .expect("Failed to run");
-
-                    print!("{}", String::from_utf8_lossy(&output.stdout));
+                    if output.stderr.is_empty() {
+                        print!("{}", String::from_utf8_lossy(&output.stdout));
+                    } else {
+                        print!("{}", String::from_utf8_lossy(&output.stderr));
+                    }
                 }
-                None => println!("{}: command not found", command),
+                None => println!("{}: command not found", input),
             }
         }
     }
